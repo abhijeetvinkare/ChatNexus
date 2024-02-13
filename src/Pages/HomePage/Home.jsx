915069e7f85b37fa2b5791db779fbb5c285e8d99
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-13 21:18:44 +0530

    initial commit

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../Components/NavBar/Navbar";
 import "./Home.css"
 import { BsSend } from "react-icons/bs";
 import right_vector_img from "../../assets/images/1707587833002_1.png"
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -13,7 +14,9 @@ function Home() {
             <div className="px-16 mt-14 flex flex-col gap-y-6 ml-10">
               <h1 className="left-heading text-6xl font-extrabold">Chat Away, Any Day<span className="left-heading-span mx-2">messages that matter</span></h1>
               <p className="text-justify font-semibold text-base home-left-div-para leading-relaxed">Chatify Your go-to messaging web app for effortless communication. Intuitive interface, vibrant chat environment. Share moments, express with emojis, join a joyful community now.</p>
+              <NavLink to="/login">
               <button className="btn-start-msg self-start p-4 w-56 font-bold text-lg flex justify-center items-center gap-x-3"><BsSend size={25}/> Start Messaging </button>
+              </NavLink>
             </div>
           </div>
           <div className="w-1/2 flex justify-center items-center mr-10">
