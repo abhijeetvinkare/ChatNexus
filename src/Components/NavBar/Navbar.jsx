6876e807a53ffc2e34cebd36ec3edfd8f05774f3
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-18 03:24:22 +0530

    initial commit

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
   }
 
   return (
-    <div className="flex justify-between items-center max-sm:px-5 max-md:px-10 px-28 py-6">
+    <div className="flex justify-between items-center max-sm:px-5 max-xl:px-10 px-28 py-6 xl:px-[90px]">
       <div>
         <h1 className="text-4xl font-bold brand-name-heading tracking-tight">
           Chatify
@@ -27,12 +27,12 @@ function Navbar() {
             <MdDarkMode size={28} onClick={handleisDark} />
           )}
         </div>
-        <div className="nav-hamburger-btn-div">
+        <div className="nav-hamburger-btn-div sm:hidden">
           <button className="nav-hamburger-btn p-2 rounded-md">
             <GiHamburgerMenu size={20} />
           </button>
         </div>
-        <div className="navbar-btn-div flex justify-around w-80 items-center">
+        <div className="navbar-btn-div max-sm:hidden flex justify-around w-80 items-center">
           <NavLink to="/login">
             <button className="btn-login-navbar py-2 px-3 w-24 font-bold rounded-md text-lg">
               Login
