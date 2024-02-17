e42045166bc140f5c74e54feb47221262e699c26
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-17 22:41:00 +0530

    initial commit

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
+import { MdLightMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
 function Navbar() {
+  const [isDark, setisDark] = useState(false);
+
+  function handleisDark() {
+    setisDark(!isDark);
+  }
+
   return (
     <div className="flex justify-between items-center max-sm:px-5 max-md:px-10 px-28 py-6">
       <div>
@@ -12,6 +20,13 @@ function Navbar() {
         </h1>
       </div>
       <div className="flex justify-around items-center">
+        <div className="mr-3 cursor-pointer">
+          {isDark ? (
+            <MdLightMode size={28} onClick={handleisDark} />
+          ) : (
+            <MdDarkMode size={28} onClick={handleisDark} />
+          )}
+        </div>
         <div className="nav-hamburger-btn-div">
           <button className="nav-hamburger-btn p-2 rounded-md">
             <GiHamburgerMenu size={20} />
@@ -24,9 +39,9 @@ function Navbar() {
             </button>
           </NavLink>
           <NavLink to="/sign-up">
-          <button className="btn-create-navbar py-3 px-5 rounded-lg font-medium hover:font-bold">
-            Create Account
-          </button>
+            <button className="btn-create-navbar py-3 px-5 rounded-lg font-medium hover:font-bold">
+              Create Account
+            </button>
           </NavLink>
         </div>
       </div>
