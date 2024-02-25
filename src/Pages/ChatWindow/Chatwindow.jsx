701f4308a6372e6bac9d24f938e0d5a8b634e651
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-25 23:53:44 +0530

    initial commit

--- src/Pages/ChatWindow/Chatwindow.jsx
+++ src/Pages/ChatWindow/Chatwindow.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Chatwindow() {
   return (
-    <div>Chatwindow</div>
-  )
+    <div className="flex justify-between">
+      <div>
+        <h1>left window</h1>
+      </div>
+      <div>
+        <h1>right window</h1>
+      </div>
+    </div>
+  );
 }
 
-export default Chatwindow
+export default Chatwindow;
