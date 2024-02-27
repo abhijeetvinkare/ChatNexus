f98b0a782986117a14069815dd6959ad753dbb70
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-27 23:32:31 +0530

    initial commit

--- src/Pages/ChatWindow/Chatwindow.jsx
+++ src/Pages/ChatWindow/Chatwindow.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Chatwindow() {
   return (
-    <div className="flex justify-between">
-      <div>
+    <div className="flex justify-between p-4">
+      <div className="w-2/5">
         <h1>left window</h1>
       </div>
-      <div>
+      <div className="w-4/5">
         <h1>right window</h1>
       </div>
     </div>
